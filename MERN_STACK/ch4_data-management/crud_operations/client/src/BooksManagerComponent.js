2d01477f0ade6352134793626338714bdf9bfc74
harshamde <harshamdo619@gmail.com>
2023-01-10 15:40:52 +0530

    ch_4 crud_operations completed

---
+++ MERN_STACK/ch4_data-management/crud_operations/client/src/BooksManagerComponent.js
@@ -0,0 +1,62 @@
+// Write some Code here
+
+import { useDispatch, useSelector } from 'react-redux';
+import booksSlice from './redux-store/BooksSlice';
+import { getBooksFromServer, saveBooksToServer } from "./redux-store/BooksThunk";
+
+function BookView({ book, index }) {
+    const dispatch = useDispatch();
+    return (
+        <div key={index} style={{ border: "1px solid black", padding: "10px", margin: "10px", display: "inline-block" }}>
+            <div>{book.name}</div>
+            <div>{book.author}</div>
+            <div>{book.price}</div>
+            <button onClick={() => dispatch(booksSlice.actions.deleteBook({ book }))}>Delete</button>
+            <button onClick={() => dispatch(booksSlice.actions.editBook({ book }))}>Edit</button>
+            <div>status: {book.status}</div>
+        </div>
+    );
+}
+
+function BookEditView({ book, index }) {
+    const dispatch = useDispatch();
+    return (
+        <div key={index} style={{ border: "1px solid black", padding: "10px", margin: "10px", display: "inline-block" }}>
+            Name: <input type="text" value={book.name} onChange={(e) => dispatch(booksSlice.actions.nameChange({ book, name: e.target.value }))} />
+            <br />
+            Author: <input type="text" value={book.author} onChange={(e) => dispatch(booksSlice.actions.authorChange({ book, author: e.target.value }))} />
+            <br />
+            Price: <input type="text" value={book.price} onChange={(e) => dispatch(booksSlice.actions.priceChange({ book, price: e.target.value }))} />
+            <br />
+            <button onClick={() => dispatch(booksSlice.actions.updateBook({ book }))}>Update</button>
+            <button onClick={() => dispatch(booksSlice.actions.cancelEditBook({ book }))}>Cancel</button>
+            <br />
+            <div>status: {book.status}</div>
+        </div>
+    );
+}
+
+export default function BooksManagerComponent() {
+    const booksSliceState = useSelector(store => store.booksSliceState);
+    const dispatch = useDispatch();
+
+    const getBooks = () => {
+        dispatch(getBooksFromServer());
+    }
+
+    return (<div> <button onClick={getBooks} >Get Books</button>
+        {booksSliceState.isLoading && <div>Please Wait ...</div>}
+        {booksSliceState.error && <div style={{ color: "red" }}>{booksSliceState.error}</div>}
+        {booksSliceState.books.map((book, index) =>
+            (book.status === "from-server" || book.status === "from-server & updated" || book.status === "new & updated")
+                ? <BookView book={book} index={index} />
+                : (book.status === "from-server & edit" || book.status === "new & edit") && <BookEditView book={book} index={index} />
+        )}
+        <br />
+        <button onClick={() => dispatch(booksSlice.actions.addNewBook())}>Add New Book</button>
+        <br />
+        {booksSliceState.isSaving
+            ? <div> "Saving..." </div>
+            : <button onClick={() => dispatch(saveBooksToServer(booksSliceState.books))}>Save To Database</button>}
+    </div>);
+}
